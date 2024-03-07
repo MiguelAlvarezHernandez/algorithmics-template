951ec00832fd30dc5f5903d793b361e4ba8c23a6
Miguel Álvarez Hernández <UO295368@uniovi.es>
2024-03-07 19:30:50 +0100

    Excel session 3 updated and zip ws

--- Practica3 2024.eng/algstudent/ws/lab03/src/algstudent/s3/Division4.java
+++ Practica3 2024.eng/algstudent/ws/lab03/src/algstudent/s3/Division4.java
@@ -18,11 +18,15 @@ public class Division4 {
 	public static void main (String arg []) 
 	{
 		 long t1,t2,cont = 0;	 
+		 int nTimes = Integer.parseInt (arg [0]);
 		 for (int n=1000;n<=10000000;n*=2)
 		 {
 			  t1 = System.currentTimeMillis ();
-			   
+			 
+			  for (int reps=1; reps<=nTimes;reps++)
+				{ 
 			  cont = rec4(n);
+				}
 			      
 			  t2 = System.currentTimeMillis ();
 			
